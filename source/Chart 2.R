acdd3952820e2e2078277e87c2a743309df24cf8
msl2002 <msl2002@uw.edu>
2022-11-14 21:48:45 -0800

    Popularity vs. Energy Chart

---
+++ source/Chart 2.R
@@ -0,0 +1,32 @@
+spotify_2000_2019 <- read.csv("C:/Users/msl4e/Documents/info201/Project/project-team-7/data/songs_normalize.csv")
+spotify_unpopular_songs <- read.csv("C:/Users/msl4e/Documents/info201/Project/project-team-7/data/unpopular_songs.csv")
+spotify_1921_2020 <- read.csv("C:/Users/msl4e/Documents/info201/Project/project-team-7/data/data.csv")
+
+library(dplyr)
+
+popular_songs <- full_join(spotify_1921_2020, spotify_2000_2019)
+all_songs <- full_join(spotify_unpopular_songs, popular_songs)
+
+spotify_1921_2020 <- spotify_1921_2020 %>% 
+  rename("song" = "name", "artist" = "artists")
+
+spotify_unpopular_songs <- spotify_unpopular_songs %>% 
+  rename("id" = "track_id", "explicit_T_F" = "explicit")
+
+new_data_energy <- all_songs %>% 
+  select(popularity, energy) %>% 
+  group_by(popularity) %>% 
+  summarize(energy = mean(energy))
+
+
+popularity_energy <- ggplot(new_data_energy, aes(x = popularity, y = energy)) +
+  geom_point(color = "blue") +
+  scale_y_continuous(labels = scales::comma) +
+  labs(
+    x = "Popularity",
+    y = "Average Energy",
+    title = "Popularity vs. Energy"
+  ) 
+plot(popularity_energy)
+
+
