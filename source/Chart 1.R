72dfe38e2e11d4ccac27e7f5cad43d750b750b2c
msl2002 <msl2002@uw.edu>
2022-11-14 21:59:04 -0800

    Chart 1 and 2 added to report

--- source/Chart 1.R
+++ source/Chart 1.R
@@ -8,14 +8,14 @@ spotify_1921_2020 <- read.csv("C:/Users/msl4e/Documents/info201/Project/project-
 
 library(dplyr)
 
-popular_songs <- full_join(spotify_1921_2020, spotify_2000_2019)
-all_songs <- full_join(spotify_unpopular_songs, popular_songs)
-
 spotify_1921_2020 <- spotify_1921_2020 %>% 
   rename("song" = "name", "artist" = "artists")
 
 spotify_unpopular_songs <- spotify_unpopular_songs %>% 
   rename("id" = "track_id", "explicit_T_F" = "explicit")
+
+popular_songs <- full_join(spotify_1921_2020, spotify_2000_2019)
+all_songs <- full_join(spotify_unpopular_songs, popular_songs)
 
 new_data <- all_songs %>% 
   select(popularity, danceability) %>% 
