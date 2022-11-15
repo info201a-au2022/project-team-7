7c30c1af9612f51f97a4ea8bdc1fdddc17883abb
sberes44 <sberes44@uw.edu>
2022-11-14 22:54:06 -0800

    Made column headers more readable

--- source/Summary_Table.R
+++ source/Summary_Table.R
@@ -1,17 +1,41 @@
 library(tidyverse)
 library(dplyr)
 
-# Installs and initializes package for inserting tables into markdown files
+# Reading 3 sets of data from .csv files
 
-install.packages("formattable")
-library(formattable)
-library(knitr)
+spotify_2000_2019 <- read.csv("./info201/Project/project-team-7/data/songs_normalize.csv")
+spotify_unpopular_songs <- read.csv("./info201/Project/project-team-7/data/unpopular_songs.csv")
+spotify_1921_2020 <- read.csv("./info201/Project/project-team-7/data/data.csv")
 
-# Pulls data from Summary_Info to gain access to summary_info table
+# Renaming columns to match all 3 sets of data
 
-source("./info201/Project/project-team-7/source/Summary_Info.R")
+spotify_1921_2020 <- spotify_1921_2020 %>% 
+  rename("song" = "name", "artist" = "artists")
+
+spotify_unpopular_songs <- spotify_unpopular_songs %>% 
+  rename("id" = "track_id", "song" = "track_name", "artist" = "track_artist")
+
+# Reassigning explicit columns across sets of data to equal logical values
+
+spotify_1921_2020$explicit <- as.logical(spotify_1921_2020$explicit)
+
+spotify_unpopular_songs$explicit <- as.logical(spotify_unpopular_songs$explicit)
+
+# Joining all 3 sets of data into both a "popular songs" set and an "all songs" set
+
+popular_songs <- full_join(spotify_1921_2020, spotify_2000_2019)
+all_songs <- full_join(spotify_unpopular_songs, popular_songs)
+
+# Averaging statistics for all songs in Spotify
+
+avg_statistics <- all_songs %>%
+  summarize_at(c("danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "duration_ms", "popularity"), mean, na.rm = TRUE)
 
 # Formats summary_info table to have a title and appear cleaner in markdown files
 
-formattable(summary_info) %>%
+summary_table <- summary_info %>%
+  rename("Danceability" = "danceability", "Energy" = "energy", "Key" = "key", "Loudness (dB)" = "loudness", "Speechiness" = "speechiness", "Acousticness" = "acousticness", "Instrumentalness" = "instrumentalness", "Liveness" = "liveness", "Valence (Positivity)" = "valence", "Tempo (BPM)" = "tempo", "Duration (ms)" = "duration_ms")
+
+
+formattable(summary_table) %>%
   kable("html", escape = F, caption = "Spotify Songs - Average Statistics", align = c("l", "r", "r", "r", "r"))
